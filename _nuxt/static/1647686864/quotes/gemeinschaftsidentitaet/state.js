df4bc357b68546a7e343104593b866a12710e7ec
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-19 12:39:41 +0000

    deploy: ac1950b868292d99131dff2bcac182ee90d8fb60

--- _nuxt/static/1647686864/quotes/gemeinschaftsidentitaet/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647686864",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquotes\u002Fgemeinschaftsidentitaet",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8f726a85"}}}}(null));
