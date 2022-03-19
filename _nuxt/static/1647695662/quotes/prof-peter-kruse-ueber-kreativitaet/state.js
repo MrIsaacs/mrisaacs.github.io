983931e6878c204915767084ea1f250a411a3cb7
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-19 13:14:24 +0000

    deploy: be8b891e4c6b87025fc95551935228f246250a76

---
+++ _nuxt/static/1647695662/quotes/prof-peter-kruse-ueber-kreativitaet/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647695662",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquotes\u002Fprof-peter-kruse-ueber-kreativitaet",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0e0bad33"}}}}(null));
