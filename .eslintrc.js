fe388baaf6fc7849b639291cf6efda5e5e194918
Ivan Ilic <me@mrisaacs.org>
2022-03-19 02:16:38 +0100

    chore: unset server side rendering property

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   ],
   plugins: [
   ],
-  // add your custom rules here
   rules: {
     "vue/multi-word-component-names": ["off"]
   }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-  ssr: true,
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
