830c4aeeaa2aa8949649d97d67e4774b63cf015c
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-19 23:00:45 +0000

    deploy: 04de113d9b50c200a068ffb0f4d3346fc3017e59

--- _nuxt/static/1647711236/memos/fuer-das-gute-kaempfen/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647711236",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemos\u002Ffuer-das-gute-kaempfen",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"64b0c590"}}}}(null));
