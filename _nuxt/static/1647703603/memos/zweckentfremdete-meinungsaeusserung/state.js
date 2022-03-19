328d3e7459d6849651dd25bdddb47c674d08d80e
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-19 15:26:45 +0000

    deploy: 055b727caed5a724ec5fdd3d3670114bb9a6e6cb

---
+++ _nuxt/static/1647703603/memos/zweckentfremdete-meinungsaeusserung/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647703603",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemos\u002Fzweckentfremdete-meinungsaeusserung",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0b962eee"}}}}(null));
