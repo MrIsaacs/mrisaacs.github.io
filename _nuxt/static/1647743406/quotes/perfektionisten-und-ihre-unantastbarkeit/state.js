6464dba4e07e7d1aef4fa6112fe8b34a226e0750
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-20 02:33:07 +0000

    deploy: 8e8b2334da34f81c0354b7e7ab40ab040f3e1334

--- _nuxt/static/1647743406/quotes/perfektionisten-und-ihre-unantastbarkeit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647743406",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquotes\u002Fperfektionisten-und-ihre-unantastbarkeit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f7155b8"}}}}(null));
---
+++ _nuxt/static/1647743585/quotes/luis-morales-de-la-cruz-im-interview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647743585",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquotes\u002Fluis-morales-de-la-cruz-im-interview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1439b29d"}}}}(null));
