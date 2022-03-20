82d3f55aca76e43dc1b6e8c9cb2d9265fe278d7a
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-20 04:33:08 +0000

    deploy: 75fe76f62c906f9b424bcaf1931389b0f56c2028

--- _nuxt/static/1647744590/memos/zurueck-zur-natur/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647744590",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemos\u002Fzurueck-zur-natur",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57c18b7e"}}}}(null));
---
+++ _nuxt/static/1647750786/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647750786",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fc4a4672"}}}}(null,"\u002F"));
