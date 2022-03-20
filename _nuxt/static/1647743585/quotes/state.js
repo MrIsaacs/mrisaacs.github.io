8d56dcd87d9f79abe0b07772bf727525ff3e7b8f
MrIsaacs <MrIsaacs@users.noreply.github.com>
2022-03-20 02:49:53 +0000

    deploy: c9d57295dea673c7934c0e8f29e1d13de8c5529c

--- _nuxt/static/1647743585/quotes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647743585",layout:"list",error:a,serverRendered:true,routePath:"\u002Fquotes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1439b29d"}}}}(null));
