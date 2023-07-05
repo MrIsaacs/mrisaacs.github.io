feb9f145c7bbd5a794f7776bd65b4dfd42d40a5a
MrIsaacs <MrIsaacs@users.noreply.github.com>
2023-07-05 13:47:28 +0000

    deploy: 57ea9ecb4cbaa1a31ca59b095266dfc859f1eefa

--- _nuxt/static/1654670354/quotes/luis-morales-de-la-cruz-im-interview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654670354",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquotes\u002Fluis-morales-de-la-cruz-im-interview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0a415c15"}}}}(null));
