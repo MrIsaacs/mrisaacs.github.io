f832289722e0d61c29b49012db38b7f67fb94d04
MrIsaacs <MrIsaacs@users.noreply.github.com>
2023-07-05 14:51:32 +0000

    deploy: 57ea9ecb4cbaa1a31ca59b095266dfc859f1eefa

---
+++ _nuxt/ProseTbody.c17bcd6d.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,p as t,r as s}from"./entry.9a5f9d5f.js";const n={};function c(e,a){return r(),t("tbody",null,[s(e.$slots,"default")])}const _=o(n,[["render",c]]);export{_ as default};
