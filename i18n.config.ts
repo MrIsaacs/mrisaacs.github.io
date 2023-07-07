167edec3f5d72e542be7fdccbcf84e513220f654
Ivan Ilic <me@mrisaacs.org>
2023-07-07 12:29:41 +0200

    add i18n.config.ts for locales
    This file is added temporarily and will be later replaced by i18n.json
    in the 'config' folder until a solution for implementing a json config
    into the 'i18n.config.ts' file is found.

---
+++ i18n.config.ts
@@ -0,0 +1,19 @@
+import de from './locales/de-AT.json'
+
+export default defineI18nConfig(() => ({
+  defaultLocale: 'de',
+  langDir: './locales',
+  locale: 'de',
+  lazy: {
+    skipNuxtState: true
+  },
+  messages: {
+    en,
+    de
+  },
+  parsePages: false,
+  strategy: 'prefix_except_default',
+  vueI18n: {
+    legacy: false
+  }
+}))
